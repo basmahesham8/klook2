5b03fe9a7c2d311a80a7b18a4b5254731e89c78e
Basma Hesham <basmahesham8@gmail.com>
2021-03-06 13:14:53 +0200

    add

--- lib/components/home.dart
+++ lib/components/home.dart
@@ -6,10 +6,44 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String recommended = 'KLOOK in cairo';
+  List recommendedCities = [
+    'KLOOK in cairo',
+    'KLOOK in london',
+    'KLOOK in Indonisia'
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('home v'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('home'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            DropdownButton(
+              value: recommended,
+              onChanged: (newCity) {
+                setState(() {
+                  recommended = newCity;
+                });
+              },
+              items: recommendedCities.map(
+                (city) {
+                  return DropdownMenuItem(
+                    value: city,
+                    child: Text(
+                      city,
+                    ),
+                  );
+                },
+              ).toList(),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
